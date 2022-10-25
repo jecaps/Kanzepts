c3637e54ffb73bab82e69ea1f3bd10845e255c4c
Jerika Kanz <jerikacapila@gmail.com>
2022-10-25 18:57:03 +0200

    Adding App Pages

---
+++ src/pages/History.js
@@ -0,0 +1,3 @@
+export default function History() {
+  return <div>History Page is still under construction</div>;
+}
---
+++ src/pages/Plan.js
@@ -0,0 +1,3 @@
+export default function Plan() {
+  return <div>Plan Page is still under construction</div>;
+}
