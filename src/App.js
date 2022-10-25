43e6db39eb48df938fe4ed911118f3618c9d6898
Jerika Kanz <jerikacapila@gmail.com>
2022-10-25 18:59:19 +0200

    Using Routers and NavLinks to navigate to different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Header from "./components/Header";
 import Nav from "./components/Nav";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Form from "./pages/Form";
+import Plan from "./pages/Plan";
+import History from "./pages/History";
 import styled from "styled-components";
 
 export default function App() {
   return (
     <Container>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="form" element={<Form />} />
+        <Route path="plan" element={<Plan />} />
+        <Route path="history" element={<History />} />
+      </Routes>
       <Nav />
     </Container>
   );
