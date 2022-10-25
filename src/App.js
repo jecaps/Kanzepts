4e51594abe74c8d94137a6addeb435743e7ea86d
Jerika Kanz <jerikacapila@gmail.com>
2022-10-25 13:01:42 +0200

    Creating and Styling Header Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+import Header from "./components/Header";
+
 import styled from "styled-components";
 
 export default function App() {
   return (
     <Container>
-      <AppHeader></AppHeader>
+      <Header />
     </Container>
   );
 }
 
 const Container = styled.div`
   text-align: center;
-`;
-
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
 `;
