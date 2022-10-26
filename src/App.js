09ec33288ad0e70abe35db3c5f80e69c3e94b1fe
Jerika Kanz <76426580+jecaps@users.noreply.github.com>
2022-10-26 11:57:00 +0200

    Improvement
    * Add Page Layout
    
    * Styling the Layout container

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import Header from "./components/Header";
-import Nav from "./components/Nav";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
 import Plan from "./pages/Plan";
 import History from "./pages/History";
 import Error from "./pages/Error";
-import styled from "styled-components";
 
 export default function App() {
   return (
-    <Container>
-      <Header />
-      <Routes>
-        <Route index path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="form" element={<Form />} />
-        <Route path="plane" element={<Plan />} />
+        <Route path="plan" element={<Plan />} />
         <Route path="history" element={<History />} />
         <Route path="*" element={<Error />} />
-      </Routes>
-      <Nav />
-    </Container>
+      </Route>
+    </Routes>
   );
 }
-
-const Container = styled.div`
-  text-align: center;
-`;
