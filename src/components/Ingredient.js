033e6e5f0dc832049277d0960b468b4cf9ff149f
Jerika Kanz <jerikacapila@gmail.com>
2022-10-27 21:38:36 +0200

    Create Ingredient component that displays ingredient amount and name in  different columns

---
+++ src/components/Ingredient.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export default function Ingredient({ name, unit, amount }) {
+  return (
+    <IngredientContainer>
+      <Amount>
+        {amount} {unit}
+      </Amount>
+      <IngredientName>{name}</IngredientName>
+    </IngredientContainer>
+  );
+}
+
+const IngredientContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  margin: auto;
+  text-align: right;
+
+  p {
+    margin: 0;
+  }
+`;
+
+const Amount = styled.p`
+  text-align: left;
+  width: 40%;
+`;
+
+const IngredientName = styled.p`
+  text-align: left;
+  width: 60%;
+`;
