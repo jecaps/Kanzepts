9dfccadc4170ee3d03e4b361424bd564c7c8243a
Jerika Kanz <jerikacapila@gmail.com>
2022-11-04 13:34:37 +0100

    Displaying schedules that have already passed

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,3 +1,37 @@
+import { nanoid } from "nanoid";
+import { useContext } from "react";
+import { RecipeContext } from "../context/RecipeContext";
+
 export default function History() {
-  return <div>History Page is still under construction</div>;
+  const { mealSchedule } = useContext(RecipeContext);
+
+  function isPast(date) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    return new Date(date) < today;
+  }
+
+  return (
+    <div>
+      {Object.keys(mealSchedule)
+        .filter((mealDate) => isPast(mealDate))
+        .map((mealDate) => (
+          <ul key={nanoid()}>
+            <p>{mealDate}</p>
+            {mealSchedule[mealDate]?.breakfast && (
+              <li>Breakfast: {mealSchedule[mealDate]?.breakfast["title"]}</li>
+            )}
+            {mealSchedule[mealDate]?.lunch && (
+              <li>Lunch: {mealSchedule[mealDate]?.lunch["title"]}</li>
+            )}
+            {mealSchedule[mealDate]?.dinner && (
+              <li>Dinner: {mealSchedule[mealDate]?.dinner["title"]}</li>
+            )}
+            {mealSchedule[mealDate]?.snacks && (
+              <li>Snacks: {mealSchedule[mealDate]?.snacks["title"]}</li>
+            )}
+          </ul>
+        ))}
+    </div>
+  );
 }
