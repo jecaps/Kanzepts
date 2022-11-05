fab849afe27a46d303051d094813ea144f2fabfb
Jerika Kanz <jerikacapila@gmail.com>
2022-11-05 16:26:50 +0100

    Create Functions for telling day and month

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -10,3 +10,56 @@ export function detailsPageNavigator(mealSetter, mealToSet) {
   window.scrollTo({ top: 0, left: 0 });
   mealSetter(mealToSet);
 }
+
+export function tellDayOfWeek(mealDate) {
+  const dayOfWeek = new Date(mealDate).getDay();
+  const today = new Date().getDay();
+
+  return dayOfWeek === today
+    ? "TODAY"
+    : dayOfWeek + 1 === today + 1 || (dayOfWeek === 0 && today + 1 === 7)
+    ? "TOMORROW"
+    : dayOfWeek - 1 === today - 1 || (dayOfWeek === 0 && today - 1 === -1)
+    ? "YESTERDAY"
+    : dayOfWeek === 0
+    ? "SUN"
+    : dayOfWeek === 1
+    ? "MON"
+    : dayOfWeek === 2
+    ? "TUE"
+    : dayOfWeek === 3
+    ? "WED"
+    : dayOfWeek === 4
+    ? "THU"
+    : dayOfWeek === 5
+    ? "FRI"
+    : "SAT";
+}
+
+export function tellMonth(mealDate) {
+  const month = new Date(mealDate).getMonth();
+
+  return month === 0
+    ? "JAN"
+    : month === 1
+    ? "FEB"
+    : month === 2
+    ? "MAR"
+    : month === 3
+    ? "APR"
+    : month === 4
+    ? "MAY"
+    : month === 5
+    ? "JUNE"
+    : month === 6
+    ? "JULY"
+    : month === 7
+    ? "AUG"
+    : month === 8
+    ? "SEP"
+    : month === 9
+    ? "OCT"
+    : month === 10
+    ? "NOV"
+    : "DEC";
+}
