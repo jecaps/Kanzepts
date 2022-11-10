1ae6ac791dbb9bb7ab83e2b830c357ce61a6e34c
Jerika Kanz <jerikacapila@gmail.com>
2022-11-10 21:25:27 +0100

    Move tests to one folder

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import data from "../data";
-import Card from "./Card";
+import Card from "../components/Card";
 
 const recipe = data[0];
 
