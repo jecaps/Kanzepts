26dbad293de31044d6d6bd31399c16e1f3159fbb
Jerika Kanz <jerikacapila@gmail.com>
2022-11-13 16:20:47 +0100

    Change date placeholder

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,14 +1,14 @@
 export default function DateInput({ setDate }) {
   return (
     <div>
-      <input type="date" id="date" onfocus="(this.type='date')" required />
       <input
         min={new Date().toISOString().substring(0, 10)}
         onChange={(e) => setDate(e.target.value)}
-        placeholder="Type Date"
+        placeholder="dd.mm.yyyy"
         type="text"
         onfocus="(this.type = 'date')"
         id="date"
+        required
       ></input>
     </div>
   );
