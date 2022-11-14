37dcf317b6029906045f1dbaf6a4ae985cc4ad31
Jerika Kanz <jerikacapila@gmail.com>
2022-11-14 15:39:11 +0100

    Change App Font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
-    font-family: 'Baloo Chettan 2', cursive;
+    font-family: 'dosis', cursive;
   }
 
   body {
