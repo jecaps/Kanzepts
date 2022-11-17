a25393a76a2958225be81bd3b0c904536e5a0c7a
Jerika Kanz <jerikacapila@gmail.com>
2022-11-17 15:47:36 +0100

    Style Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -21,24 +21,45 @@ export default function Nav() {
   );
 }
 const Navbar = styled.nav`
-  background-color: #ff9c08;
+  background-color: #e0e1dd;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   position: fixed;
-  bottom: 0;
-  width: 100%;
+  border-radius: 1.5rem;
+  overflow: hidden;
+  bottom: 0.75rem;
+  width: 90%;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 10px 100px 80px rgba(0, 0, 0, 0.07),
+    0px 41.7776px 33.4221px rgba(0, 0, 0, 0.0503198),
+    0px 22.3363px 17.869px rgba(0, 0, 0, 0.0417275),
+    0px 12.5216px 10.0172px rgba(0, 0, 0, 0.035),
+    0px 6.6501px 5.32008px rgba(0, 0, 0, 0.0282725),
+    0px 2.76726px 2.21381px rgba(0, 0, 0, 0.0196802);
 
   .active {
-    background-color: #7d1100;
+    background-color: #273043;
+
+    svg {
+      fill: #30cfd0;
+    }
   }
 
   a {
-    padding: 1rem;
+    padding: 0.5rem;
     width: 100%;
   }
 
   svg {
-    width: 25px;
-    fill: #db4200;
+    width: 1.25rem;
+    fill: #273043;
+    transition: 0.3s;
+
+    &:active,
+    &:hover {
+      transform: scale(1.25);
+    }
   }
 `;
